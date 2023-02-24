c03c2ce52dbe3458605e761839d6a8c76d5139b8
dustinswales <dswales@ucar.edu>
2023-02-24 14:04:41 -0700

    working with GFS p8 physics

--- scm/src/suite_info.py
+++ scm/src/suite_info.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 class suite(object):
-  
+
     DEFAULT_MAX_TIMESTEP = 1800.0
-  
+
     def __init__(self, name, tracers, namelist, timestep, max_timestep, supported):
         DEFAULT_MAX_TIMESTEP = 1800.0
         self._name = name                     #should remain unchanged after init
@@ -12,12 +12,12 @@ class suite(object):
         self.tracers = self._default_tracers   #can be modified after init
         self.namelist = self._default_namelist #can be modified after init
         self._supported = supported
-      
+
         if max_timestep > 0:
             self._max_timestep = max_timestep     #should remain unchanged after init
         else:
             self._max_timestep = DEFAULT_MAX_TIMESTEP
-        
+
         if timestep <= self._max_timestep and timestep > 0:
             self._default_timestep = timestep #should remain unchanged after init
             self.timestep = self._default_timestep #can be modified after init
@@ -26,7 +26,7 @@ class suite(object):
         else:
             message = 'The timestep for suite {0} cannot be set greater than the max_timestep of {1}'.format(self._name, self._max_timestep)
             raise Exception(message)
-      
+
         @property
         def timestep(self):
             """Get the timestep for the given suite."""
@@ -40,7 +40,7 @@ class suite(object):
             else:
                 message = 'The timestep for suite {0} cannot be set greater than the max_timestep of {1}'.format(self._name, self._max_timestep)
                 raise Exception(message)
-      
+
 suite_list = []
 suite_list.append(suite('SCM_GFS_v16',           'tracers_GFS_v16.txt',                  'input_GFS_v16.nml',                 600.0, 1800.0, True ))
 suite_list.append(suite('SCM_GFS_v17_p8_ugwpv1', 'tracers_GFS_v17_p8.txt',               'input_GFS_v17_p8_ugwpv1.nml',       600.0, 600.0,  True ))
@@ -52,6 +52,7 @@ suite_list.append(suite('SCM_GFS_v16_RRTMGP',    'tracers_GFS_v16.txt',         
 suite_list.append(suite('SCM_GFS_v17_p8',        'tracers_GFS_v17_p8.txt',               'input_GFS_v17_p8.nml',              600.0, 600.0,  False))
 suite_list.append(suite('SCM_HRRR',              'tracers_HRRR.txt',                     'input_HRRR.nml',                    600.0, 600.0 , False))
 suite_list.append(suite('SCM_RRFS_v1beta',       'tracers_RRFS_v1beta.txt',              'input_RRFS_v1beta.nml',             600.0, 600.0 , False))
+suite_list.append(suite('SCM_GFS_v17_p8_mmm',    'tracers_GFS_v17_p8.txt',               'input_GFS_v17_p8_mmm.nml',          600.0, 600.0,  False))
 suite_list.append(suite('SCM_GFS_v15p2',         'tracers_GFS_v15p2.txt',                'input_GFS_v15p2.nml',               600.0, 1800.0, False))
 suite_list.append(suite('SCM_GFS_v15p2_RRTMGP',  'tracers_GFS_v15p2.txt',                'input_GFS_v15p2_RRTMGP.nml',        600.0, 1800.0, False))
 suite_list.append(suite('SCM_GFS_v15p2_no_nsst', 'tracers_GFS_v15p2.txt',                'input_GFS_v15p2.nml',               600.0, 1800.0, False))
@@ -69,14 +70,13 @@ suite_list.append(suite('SCM_RRFS_v1nssl',       'tracers_RRFS_v1nssl_nohail_noc
 suite_list.append(suite('SCM_csawmg',            'tracers_csawmg.txt',                   'input_csawmg.nml',                  600.0, 1800.0, False))
 
 def main():
-    
+
     #print supported suites separated by commas
     suite_string = ''
     for s in suite_list:
         if s._supported:
-            suite_string += s._name + ',' + s._name + '_ps' + ','  
+            suite_string += s._name + ',' + s._name + '_ps' + ','
     print(suite_string[:-1])
 
 if __name__ == '__main__':
-    main()  
- 
+    main()
