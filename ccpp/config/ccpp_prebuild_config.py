b48c00b1b0007f84a9c319ca7a3c7eaee3adf71c
Weiwei <weiweili@ucar.edu>
2023-09-25 12:33:05 -0600

    Modified code to connect MMM surface layer scheme to CCPP SCM
    	modified:   ccpp/config/ccpp_prebuild_config.py
    	modified:   ccpp/physics
    	modified:   ccpp/physics_namelists/input_RAP_mmm.nml
    	new file:   ccpp/physics_namelists/input_RAP_sfclayrev.nml
    	modified:   ccpp/suites/suite_SCM_RAP_mmm.xml
    	new file:   ccpp/suites/suite_SCM_RAP_sfclayrev.xml
    	modified:   scm/src/CCPP_typedefs.F90
    	modified:   scm/src/CCPP_typedefs.meta
    	modified:   scm/src/GFS_typedefs.F90
    	modified:   scm/src/GFS_typedefs.meta
    	modified:   scm/src/suite_info.py
    
    	modified:   ../../ccpp/config/ccpp_prebuild_config.py
    	modified:   ../../ccpp/physics
    	modified:   ../../ccpp/physics_namelists/input_RAP_mmm.nml
    	new file:   ../../ccpp/physics_namelists/input_RAP_sfclayrev.nml
    	modified:   ../../ccpp/suites/suite_SCM_RAP_mmm.xml
    	new file:   ../../ccpp/suites/suite_SCM_RAP_sfclayrev.xml
    	modified:   CCPP_typedefs.F90
    	modified:   CCPP_typedefs.meta
    	modified:   GFS_typedefs.F90
    	modified:   GFS_typedefs.meta
    	modified:   suite_info.py

--- ccpp/config/ccpp_prebuild_config.py
+++ ccpp/config/ccpp_prebuild_config.py
@@ -224,11 +224,12 @@ SCHEME_FILES = [
     'ccpp/physics/physics/tools/get_prs_fv3.F90',
     'ccpp/physics/physics/tools/get_phi_fv3.F90'
     # NCAR MMM
+    'ccpp/physics/physics/gfs_mmm_sfclayrev_pre.F90'        ,
+    'ccpp/physics/physics/gfs_mmm_sfclayrev_post.F90'       ,
     'ccpp/physics/physics/gfs_mmm_ysu_pre.F90'              ,
     'ccpp/physics/physics/gfs_mmm_ysu_post.F90'             ,
-    'ccpp/physics/physics/mmm_physics/bl_ysu.F90'           ,
-    'ccpp/physics/physics/mmm_gwdo.F90'                     ,
-    'ccpp/physics/physics/mmm_sfclay.F90'
+    'ccpp/physics/physics/mmm_physics/sf_sfclayrev.F90'     ,
+    'ccpp/physics/physics/mmm_physics/bl_ysu.F90'
     ]
 
 # Default build dir, relative to current working directory,
