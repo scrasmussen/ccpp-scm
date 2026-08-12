a21aaf52c81c82e4d0853acfd50619bcaff44507
Grant Firl <grant.firl@noaa.gov>
2026-08-12 15:20:37 -0400

    grab new tempo_v3 files from new release asset

--- contrib/get_tempo_data.sh
+++ contrib/get_tempo_data.sh
@@ -3,7 +3,7 @@
 # Function to display help message
 print_help() {
     echo "get_tempo_data.sh: contrib/get_tempo_data.sh [-v,--verbose]"
-    echo "    Script for downloading/extracting the TEMPO lookup tables."
+    echo "    Script for downloading/extracting the TEMPO v3 lookup tables."
     echo ""
     echo "Options:"
     echo "    -v, --verbose    Turn on wget verbose output."
@@ -46,20 +46,13 @@ BASEDIR=$MYDIR/..
 # Change to directory containing the physics input data, download and extract archive
 mkdir -p $BASEDIR/scm/data/physics_input_data/
 cd $BASEDIR/scm/data/physics_input_data/
-wget ${verbose} https://github.com/NCAR/ccpp-scm/releases/download/v7.0.0/tempo_data.tar.gz
-tar -xvzf tempo_data.tar.gz
-rm -f tempo_data.tar.gz
+wget ${verbose} https://github.com/NCAR/ccpp-scm/releases/download/v7.0.0/tempo_v3_data.tar.gz
+tar -xvzf tempo_v3_data.tar.gz
+rm -f tempo_v3_data.tar.gz
 
-# The archive uses legacy MP_TEMPO_*.dat names; tempo_v3's Fortran source
-# (module_mp_tempo_cfgs.F90) hardcodes different filenames with no
-# host/namelist override, so symlink to what it actually looks for.
-#
 # qr_acr_qg_data_tempo_v3 -> HAILAWARE variant: workaround for an upstream
 # bug (see claude_bug_report.md) where initialize_arrays_qr_acr_qg() always
 # allocates the hail-aware-sized (nrhg=9) table regardless of hailaware_flag.
-ln -sf MP_TEMPO_HAILAWARE_QRacrQG.dat qr_acr_qg_data_tempo_v3
-ln -sf MP_TEMPO_QRacrQS.dat           qr_acr_qs_data_tempo_v3
-ln -sf MP_TEMPO_freezeH2O.dat         freeze_water_data_tempo_v3
 
 # ccn_activate.bin isn't in the downloaded archive at all -- it's small
 # (~35KB) and checked directly into the TEMPO submodule, so symlink it in
